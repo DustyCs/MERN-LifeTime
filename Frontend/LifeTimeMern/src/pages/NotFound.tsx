97972ba8b3cf9bf3a196c925e86b387f09c403d8
DustyC <123617229+DustyCs@users.noreply.github.com>
2025-03-14 23:10:31 +0800

    Updated Frontend

---
+++ Frontend/LifeTimeMern/src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='flex justify-center items-center'>
+        <h1 className='text-3xl font-bold'>404 Page Does Not Exists</h1>
+    </div>
+  )
+}
